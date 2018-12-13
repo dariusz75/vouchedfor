a203fcdf31515f3fa86becfe0beeb08c16d684bb
Dariusz Franczak <darius32@op.pl>
2018-12-13 21:46:10 +0000

    data fetched

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 import Calculator from './Calculator';
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get('dummyData.json').then((response) => {
+      console.log(response);
+    })
+  }
+
   render() {
     return (
       <div className="App">
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class Calculator extends Component {
   render() {
     return <div className="row">
