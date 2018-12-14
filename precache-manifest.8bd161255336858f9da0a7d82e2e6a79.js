f3f08ec1318af22c414ae476682fc78a0e665525
Dariusz Franczak <darius32@op.pl>
2018-12-14 01:36:30 +0000

    Updates

---
+++ precache-manifest.8bd161255336858f9da0a7d82e2e6a79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3fee73c93b0d1dcd5d4",
+    "url": "/vouchedfor/static/js/runtime~main.c3fee73c.js"
+  },
+  {
+    "revision": "2f6c3c6e1abf13be5c4e",
+    "url": "/vouchedfor/static/js/main.2f6c3c6e.chunk.js"
+  },
+  {
+    "revision": "86c2d8530ba83b27947d",
+    "url": "/vouchedfor/static/js/1.86c2d853.chunk.js"
+  },
+  {
+    "revision": "2f6c3c6e1abf13be5c4e",
+    "url": "/vouchedfor/static/css/main.09216109.chunk.css"
+  },
+  {
+    "revision": "86c2d8530ba83b27947d",
+    "url": "/vouchedfor/static/css/1.8188009e.chunk.css"
+  },
+  {
+    "revision": "ebcf022db1a2f783073e8821267364d5",
+    "url": "/vouchedfor/index.html"
+  }
+];
